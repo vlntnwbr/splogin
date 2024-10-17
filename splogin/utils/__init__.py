57a0badf6785b64e1f7d2ac56b5c43d70c2b3889
Valentin Weber <dev@vweber.eu>
2024-10-17 21:28:36 +0200

    ADD | utility to install playwright browser during installation

--- splogin/utils/__init__.py
+++ splogin/utils/__init__.py
@@ -1,7 +1,11 @@
 """Package for utilities used by splogin."""
 
-
 import logging
+import pathlib
+import subprocess
+import sys
+
+from .errors import BrowserUnavailableError
 
 
 def get_logger(name: str, level: int | str) -> logging.Logger:
@@ -25,3 +29,21 @@ def log_error(
     """Error log the (exception) message. Debug log the traceback."""
     log.error(exc if message is None else message)
     log.debug(exc.__class__, exc_info=True)
+
+
+def playwright_install(browser: str = "firefox") -> None:
+    """Install browser using playwright cli in the python directory."""
+    playwright_bin = pathlib.Path(sys.executable).parent / "playwright"
+    if isinstance(playwright_bin, pathlib.WindowsPath):
+        playwright_bin = playwright_bin.with_suffix(".exe")
+    if not playwright_bin.is_file():
+        raise BrowserUnavailableError(
+            "Failed to locate binary for playwright cli",
+            playwright_bin
+        )
+    try:
+        subprocess.run((playwright_bin, "install", browser), check=True)
+    except subprocess.CalledProcessError as exc:
+        raise BrowserUnavailableError(
+            "Failed to install browser for playwright"
+        ) from exc
